14ec774b67449515d6e59c127028f16c330c41e5
oAndin <00andherson@gmail.com>
2023-07-12 10:11:04 -0300

    Calc Year done

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,38 @@
 console.log("Hello World!");
 
 let date = new Date();
-
 console.log(date);
 
-let year;
 
- 
-let daysMonths = [
-    { january: 31 }, 
-    { february: 28 }, 
-    { march: 31 }, 
-    { april: 30 }, 
-    { may: 31 }, 
-    { juny: 30 }, 
-    { july: 31 }, 
-    { august: 31 }, 
-    { september: 30 }, 
-    { october: 31 }, 
-    { november: 30 }, 
-    { december: 31 },
-];
+let todaysYear = date.getFullYear(); // get the year
+let todaysMonth = date.getMonth(); // get the month, starting from 0
+let todaysDay = date.getDate(); // get the day of the month instead of the day of week
 
+console.log(todaysYear);
+console.log(todaysMonth);
+console.log(todaysDay);
+
+
+ // array with objects in case of select input type
+// let daysMonths = [
+//     { january: 31 }, 
+//     { february: 28 }, 
+//     { march: 31 }, 
+//     { april: 30 }, 
+//     { may: 31 }, 
+//     { juny: 30 }, 
+//     { july: 31 }, 
+//     { august: 31 }, 
+//     { september: 30 }, 
+//     { october: 31 }, 
+//     { november: 30 }, 
+//     { december: 31 },
+// ];
+
+// leap year function
 function leapYear(){
-    if ( year % 4 === 0 ) {
-
-    }
-    else {
-
+    if ( todaysYear % 4 === 0  ) {
+        console.log("Yes, leap year");
     }
 }
 
@@ -35,15 +40,21 @@ leapYear();
 
 calcBtn.onclick = function calc() {
     event.preventDefault();
-
-    parseInt()
-
-    console.log(day.value);
-    console.log(month.value);
-    console.log(year.value);
+    
+    let inputDay; 
+    inputDay = day.value;
+    let inputMonth; 
+    inputMonth = month.value;
+    let inputYear; 
+    inputYear = year.value;
+    
+    let calcYear;
+    calcYear = todaysYear - inputYear;  
 
     // write the exact day, month and year!
-    yearResult.innerHTML = `${year.value}`;
+    yearResult.innerHTML = `${calcYear}`;
     monthResult.innerHTML = `${month.value}`;
     dayResult.innerHTML = `${day.value}`;
 };
+
+
