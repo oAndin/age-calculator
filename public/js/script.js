06d977c4690f02cda7705a72b2ed0dc31ba69c00
oAndin <00andherson@gmail.com>
2023-07-15 02:46:44 -0300

    Parcial check script update

--- public/js/script.js
+++ public/js/script.js
@@ -23,38 +23,92 @@ console.log("Hello World!");
 // }
 
 let date = new Date();
-// Okay
 
+let todaysYear = date.getFullYear();
+let todaysMonth = date.getMonth();
+let todaysDay = date.getDate();
+
+// 1º focus in to create a variable so i dont get the error, cause you can resign a value for it later!"
+
+let dayInput;
+let monthInput;
+let yearInput;
+
+let correctInputDay;
+let correctInputMonth;
+let correctInputYear;
+// 2º store the value of it input day, month and year"(.value)
+
+day.onkeyup = function () {
+    dayInput = Number(day.value);
+    return dayInput;
+}
+month.onkeyup = function () {
+    monthInput = Number(month.value);
+    return monthInput;
+}
+
+year.onkeyup = function () {
+    yearInput = Number(year.value);
+    return yearInput;
+}
+
+// 2º focus out or blur to have a variable to check on a IF ELSE for valid data 
+day.onblur = function () {
+    if (dayInput != undefined && dayInput != null) {
+        correctInputDay = true;
+        console.log(correctInputDay);
+        return correctInputDay;
+    } else {
+        alert("Olha o vacilo");
+    };
+}
+month.onblur = function () {
+    if (dayInput != undefined && dayInput != null) {
+        correctInputMonth = true;
+        return correctInputMonth;
+    } else {
+        alert("Olha o vacilo");
+    };
+}
+year.onblur = function () {
+    if (dayInput != undefined && dayInput != null) {
+        correctInputYear = true;
+        return correctInputYear;
+    }
+    else {
+        alert("Olha o vacilo");
+    }
+}
+// 3º
 
 
 function calc() {
     event.preventDefault();
-
-    checkInputDay()
-    checkInputMonth()
-    checkInputYear()
-    
-    // if (correctInputDay === true & correctInputMonth === true && correctInputYear === true) {}
-    // else {alert('Please fill in all fields!');}
+    console.log(correctInputDay);
+    if (correctInputDay === true & correctInputMonth === true && correctInputYear === true) {
         let calcYear;
-        calcYear = todaysYear - year.value;
-        let calcMonth;
-        calcMonth = (todaysMonth - month.value) + 1;
-        let calcDay;
-        calcDay =  todaysDay - day.value;
+    calcYear = todaysYear - year.value;
+    let calcMonth;
+    calcMonth = (todaysMonth - month.value) + 1;
+    let calcDay;
+    calcDay = todaysDay - day.value;
 
-        if (calcDay < 0) {
-            calcDay *= -1;
-        }
+    if (calcDay < 0) {
+        calcDay *= -1;
+    }
 
-        if (calcMonth < 0) {
-            calcMonth *= -1;
-        }
+    if (calcMonth < 0) {
+        calcMonth *= -1;
+    }
 
     // write the users's age!
-        yearResult.innerHTML = `${calcYear}`;
-        monthResult.innerHTML = `${calcMonth}`;
-        dayResult.innerHTML = `${calcDay}`;
+    yearResult.innerHTML = `${calcYear}`;
+    monthResult.innerHTML = `${calcMonth}`;
+    dayResult.innerHTML = `${calcDay}`;
+
+    }
     
+    // else {alert('Please fill in all fields!');}
 };
 
