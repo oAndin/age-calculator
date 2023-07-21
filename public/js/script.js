20d997e1a11e79f85a30bc1c781044ec7b0d04ac
oAndin <00andherson@gmail.com>
2023-07-21 12:51:03 -0300

    Responsive fixed

--- public/js/script.js
+++ public/js/script.js
@@ -32,80 +32,68 @@ let todaysYear = date.getFullYear();
 let todaysMonth = date.getMonth();
 let todaysDay = date.getDate();
 
-let dayInput;
-let monthInput;
-let yearInput;
-
 let correctInputDay;
 let correctInputMonth;
 let correctInputYear;
 
-day.onkeyup = function () {
-    dayInput = Number(day.value);
-    return dayInput;
-}
-month.onkeyup = function () {
-    monthInput = Number(month.value);
-    return monthInput;
-}
-
-year.onkeyup = function () {
-    yearInput = Number(year.value);
-    return yearInput;
-}
-
 day.onblur = function () {
-    if (isNaN(dayInput) != true && dayInput != '' && dayInput.value >= 1) { //----------------------------------------------------------------------------
+    if (isNaN(day.value) != true && day.value != '' && day.value >= 1) { //----------------------------------------------------------------------------
         correctInputDay = true;
         console.log(correctInputDay);
         return correctInputDay;
     }
-    if (dayInput === '') {
+    if (day.value <= 0) {
+        console.log(day.value)
         day.style.borderColor = '#ff0000';
         dayLabel.style.color = '#ff0000';
         pRequiredDay.style.visibility = 'visible';
     }
-    if (isNaN(dayInput) == true || dayInput.value <= 0) {
-        day.style.borderColor = '#ff0000';
-        dayLabel.style.color = '#ff0000';
-        pValidDay.style.visibility = 'visible';
-    }
+    // if (isNaN(day.value) == true || day.value <= 0) {
+    //     day.style.borderColor = '#ff0000';
+    //     dayLabel.style.color = '#ff0000';
+    //     pValidDay.style.visibility = 'visible';
+    // }
 }
 month.onblur = function () {
-    if (isNaN(monthInput) != true && monthInput != '' && monthInput.value >= 1) {
+    if (isNaN(month.value) != true && month.value != '' && month.value >= 1) {
         correctInputMonth = true;
         return correctInputMonth;
     }
-    if (monthInput == '') {
+    if (month.value == '') {
         month.style.borderColor = '#ff0000';
         monthLabel.style.color = '#ff0000';
         pRequiredMonth.style.visibility = 'visible';
     }
-    if (isNaN(monthInput) == true || monthInput.value < 1) {
+    if (isNaN(month.value) == true || month.value < 1) {
         month.style.borderColor = '#ff0000';
         monthLabel.style.color = '#ff0000';
         pValidMonth.style.visibility = 'visible';
     }
 }
 year.onblur = function () {
-    if (isNaN(yearInput) != true && dayInput != '') {
+    if (isNaN(year.value) != true && year.value != '' && Number(year.value) < todaysYear) {
         correctInputYear = true;
         return correctInputYear;
     }
-    if (yearInput == '') {
-        year.style.borderColor = '#ff0000';
-        yearLabel.style.color = '#ff0000';
-        pRequiredYear.style.visibility = 'visible';
-    }
-    if (isNaN(yearInput) == true) {
+    if (isNaN(year.value) === true) {
         year.style.borderColor = '#ff0000';
         yearLabel.style.color = '#ff0000';
         pValidYear.style.visibility = 'visible';
     }
+    if (year.value === '') {
+        year.style.borderColor = '#ff0000';
+        yearLabel.style.color = '#ff0000';
+        pRequiredYear.style.visibility = 'visible';
+    }
+    if (Number(year.value) > todaysYear) {
+        year.style.borderColor = '#ff0000';
+        yearLabel.style.color = '#ff0000';
+        pValidYear.style.visibility = 'visible';
+        pValidYear.innerHTML = 'Must be in the past';
+    }
 }
 // --------------------------------------------------- 
 day.onfocus = function () {
-    dayInput = '';
     day.style.borderColor = '#716f6f';
     dayLabel.style.color = '#716f6f';
     pRequiredDay.style.visibility = 'hidden';
@@ -113,14 +101,12 @@ day.onfocus = function () {
 
 }
 month.onfocus = function () {
-    monthInput = '';
     month.style.borderColor = '#716f6f';
     monthLabel.style.color = '#716f6f';
     pRequiredMonth.style.visibility = 'hidden';
     pValidMonth.style.visibility = 'hidden';
 }
 year.onfocus = function () {
-    yearInput = '';
     year.style.borderColor = '#716f6f';
     yearLabel.style.color = '#716f6f';
     pRequiredYear.style.visibility = 'hidden';
